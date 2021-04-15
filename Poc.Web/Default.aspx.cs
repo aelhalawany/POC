e70c4632656e3391f237561c01be6d81156a0b76
aelhalawany <ahmed.elhalawany@gmail.com>
2021-04-15 17:33:53 +0300

    add web project to consume BLL

---
+++ Poc.Web/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Poc.Bll;
+using Poc.Model;
+using Poc.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Poc.Web
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnitOfWork unitOfWork = Entities.UOW;
+
+            Category category = new Category();
+            category.Name = "cat 19";
+
+            Product product = new Product();
+            product.Category = category;
+            product.Name = "product 19";
+            product.Price = 9.5;
+            product.ProductId = Guid.NewGuid();
+
+            unitOfWork.Repository<Product>().Add(product);
+            unitOfWork.SaveChanges();
+        }
+    }
+}
---
+++ Poc.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Poc.Web.Global" Language="C#" %>
